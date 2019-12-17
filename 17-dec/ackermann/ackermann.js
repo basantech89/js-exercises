b86357757c974bb3945b0872f3046d64bdb03450
Basant Soni <basantech89@gmail.com>
2019-12-17 18:45:06 +0530

    Dec 17 exercise

---
+++ 17-dec/ackermann/ackermann.js
@@ -0,0 +1,18 @@
+
+function ackermann({ m, n }) {
+  if (m > 0) {
+    if (n > 0) {
+      return ackermann({ m: m - 1, n: ackermann({ m, n: n - 1 }) });
+    } if (n === 0) {
+      return ackermann({ m: m - 1, n: 1 });
+    }
+  }
+  if (m === 0) {
+    return n + 1;
+  }
+  return -1;
+}
+
+export {
+  ackermann,
+};
